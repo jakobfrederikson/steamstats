2460c964239514c0583834b0ccdce3955a503b23
Jakob <jakobfred1999@gmail.com>
2026-01-12 20:50:52 +1300

    Use Bootstrap 5.3.8

--- stats/views.py
+++ stats/views.py
@@ -6,4 +6,9 @@ from stats import services
 # Create your views here.
 def index(request):
     steamid = 76561197999242706
-    return HttpResponse(services.get_steam_player_summary(steamid))
+    user_summary = services.get_steam_player_summary(steamid).json()
+
+    context = { 
+        'user_summary': user_summary
+    }
+    return render(request, 'index.html', context=context)
