bc91bbc07da1aab14ff4337783fb6af67cd899ed
Jakob <jakobfred1999@gmail.com>
2026-01-14 19:14:48 +1300

    Add Get OwendGames API call and change the return value in services for player summary

--- stats/services.py
+++ stats/services.py
@@ -1,19 +1,45 @@
 import requests
 from django.conf import settings
 
+steam_api_key = settings.STEAM_API_KEY
+format = "json"
+
 def get_steam_player_summary(steam_id):
     """
-    Returns a .json() (dict) of a steam users summary
+    Returns the dictionary of the steam users summary.
     
     :param steam_id: The user's steam ID as a number
     """
     params = {
-        'key': settings.STEAM_API_KEY,
-        'steamids': steam_id
+        'key': steam_api_key,
+        'steamids': steam_id,
+        'format': format
     }
 
     response = requests.get('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/', params)
 
+    if response.status_code == requests.codes.ok:
+        json_response = response.json()
+
+        # send in a workable state to the views
+        # don't want too much business logic being done in views
+        return json_response['response']['players'][0]
+    else:
+        return response.status_code
+    
+def get_steam_user_owned_games(steam_id):
+    """
+    Returns the json object result of GetOwnedGames from the steam API if the user's steam ID is valid.
+    
+    :param steam_id: the users steam ID
+    """
+    params = {
+        'key': steam_api_key,
+        'steamid': steam_id,
+        'format': format
+    }
+
+    response = requests.get('https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/', params)
     if response.status_code == requests.codes.ok:
         return response.json()
     else:
--- stats/views.py
+++ stats/views.py
@@ -6,15 +6,14 @@ from stats import services
 # Create your views here.
 def index(request):
     steamid = 76561197999242706
-    user_summary_json = services.get_steam_player_summary(steamid)
+    player_summary = services.get_steam_player_summary(steamid)
+    owned_games = services.get_steam_user_owned_games(steamid)
 
-    # steam returns a list of 'players' dicts, we are only requesting one player at a time.
-    steam_user_data = user_summary_json.get('response', {}).get('players', [None])[0]
-    persona_name = steam_user_data['personaname']
-    profile_url = steam_user_data['profileurl']
-
-    context = { 
-        'user_summary': steam_user_data,
+    persona_name = player_summary['personaname']
+    profile_url = player_summary['profileurl']
+    
+    context = {
+        'user_summary': player_summary,
         'persona_name': persona_name,
         'profile_url': profile_url
     }
