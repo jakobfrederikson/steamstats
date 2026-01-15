452c3da817025fd70af85fcd6db2771558d7bddc
Jakob <jakobfred1999@gmail.com>
2026-01-15 19:59:48 +1300

    Cloned a SteamID converter for ID conversion assistance

--- stats/views.py
+++ stats/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 
 from stats import services
 
+from stats.SteamIDConverterPython.SteamID import SteamID
+
 # Create your views here.
 def index(request):
-    steamid = 76561197999242706
-    player_summary = services.get_steam_player_summary(steamid)
-    owned_games = services.get_steam_user_owned_games(steamid)
+    #steamid = 76561197999242706
+    steam_id = SteamID("76561197999242706")
+    print(steam_id.get_steam_id())
+    print(steam_id.get_steam32_id())
+    print(steam_id.get_steam64_id())
+    print(steam_id.get_steam_id3())
+    # player_summary = services.get_steam_player_summary(steamid)
+    # owned_games = services.get_steam_user_owned_games(steamid)
 
     persona_name = player_summary['personaname']
     profile_url = player_summary['profileurl']
