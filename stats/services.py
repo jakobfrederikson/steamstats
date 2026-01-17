d9714e1a5f77f9d455d6a6d7854bcee0d2715772
Jakob <jakobfred1999@gmail.com>
2026-01-18 11:32:16 +1300

    Add support for getting players steam level

--- stats/services.py
+++ stats/services.py
@@ -5,6 +5,9 @@ from .models import PlayerSummaryDTO, OwnedGamesDTO
 steam_api_key = settings.STEAM_API_KEY
 format = "json"
 
+# TODO:
+# 1. add support for https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=steam_api_key&vanityurl={UsersCustomURL}
+# 2. response looks as so: {"response":{"steamid":"76561198087999365","success":1}}
 def get_steam_player_summary(steam_id64):
     """
     Returns a `PlayerSummaryDTO` object produced by the JSON response of ISteamUser/GetPlayerSummaries/v0002/.
@@ -25,6 +28,27 @@ def get_steam_player_summary(steam_id64):
         player_summary_dto = PlayerSummaryDTO.from_dict(json_response['response']['players'][0])
 
         return player_summary_dto
+    else:
+        return response.status_code
+    
+def get_steam_player_level(steam_id64):
+    """
+    Returns a an `int player_level` produced by the JSON response of IPlayerService/GetSteamLevel/v1/.
+    
+    :param steam_id64: The 64 bit version of the user's steam id.
+    """
+    params = {
+        'key': steam_api_key,
+        'steamid': steam_id64,
+        'format': format
+    }
+
+    response = requests.get('http://api.steampowered.com/IPlayerService/GetSteamLevel/v1/', params)
+
+    if response.status_code == requests.codes.ok:
+        json_response = response.json()
+        player_level = int(json_response['response']['player_level'])
+        return player_level
     else:
         return response.status_code
     
