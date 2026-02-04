55b613019c857773ff83d5d6681c2cd15fc355ab
Jakob <jakobfred1999@gmail.com>
2026-02-04 19:05:16 +1300

    Update: Remove bootstrap table and use django DRM + pagination for speed

--- stats/urls.py
+++ stats/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
      path('', views.index, name='index'),
      path('detail/<str:steam_id>', views.detail, name='detail'),
-     path('database/', views.database, name='database'),
+     path('database/', views.GameInformationListView.as_view(), name='database'),
      path('findsteamid/', views.find_steam_id, name='find_steam_id'),
 ]
